3f968fff1de5f75b454bec3af7f867afb9a31e11
Novs Yama <frost-tb-voo@users.noreply.github.com>
2018-12-19 23:13:45 +0900

    Modify default path of the target directory.

--- run.sh
+++ run.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
 DIR=`pwd`
-#TARGET=/home
-TARGET1=/mnt/sdb1fs
-TARGET2=/mnt/sdc1fs
+TARGET=/home
 
 sudo -E docker run --rm -it \
  -v ${DIR}/fileanalysis:/fileanalysis:rw \
  -v ${DIR}/view:/view:rw \
- -v ${TARGET1}:${TARGET1}:rw \
- -v ${TARGET2}:${TARGET2}:rw \
+ -v ${TARGET}:${TARGET}:rw \
  -w /fileanalysis \
  python:2.7.14 \
  ./run-media-indexer.sh
